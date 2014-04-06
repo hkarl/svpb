888c6baa9eb6260d8cb9db1d0c1c7c0044599544
Holger Karl <hkarl@ieee.org>
2014-04-06 12:55:45 +0200

    More otions and power in the admin view

--- arbeitsplan/admin.py
+++ arbeitsplan/admin.py
@@ -15,13 +15,32 @@ from arbeitsplan.models import Aufgabengruppe, Aufgabe, Meldung, Zuteilung, Leis
 
 class ZuteilungAdmin (admin.ModelAdmin):
     exclude = ('automatisch', )
+    list_display = 'ausfuehrer', 'aufgabe'
+    list_filter = ('ausfuehrer', 'aufgabe')
     
+class AufgabeAdmin (admin.ModelAdmin):
+    date_hierarchy = "datum"
+    list_display = 'aufgabe', 'anzahl', 'datum',  'verantwortlich'
+    list_filter = ('aufgabe', 'verantwortlich')
+
+class MeldungAdmin (admin.ModelAdmin):
+    date_hierachy = 'erstellt'
+    list_display = 'melder', 'aufgabe'
+    list_filter = ('melder', 'aufgabe')
+
+class LeistungAdmin (admin.ModelAdmin):
+    date_hierarchy = 'erstellt'
+    list_display = ('melder', 'aufgabe', 'wann',  'status')
+    list_filter = ('aufgabe__verantwortlich', 'status', 'aufgabe', 'wann',  )
+    radio_fields = {'status': admin.HORIZONTAL}
+    search_fields = ('melder__username',)
+
 ## admin.site.unregister(User)
 ## admin.site.register (User, UserAdmin)
 
 admin.site.register (Zuteilung, ZuteilungAdmin)
 admin.site.register(Aufgabengruppe)
-admin.site.register(Aufgabe)
-admin.site.register(Meldung)
-admin.site.register(Leistung) 
+admin.site.register(Aufgabe, AufgabeAdmin)
+admin.site.register(Meldung, MeldungAdmin)
+admin.site.register(Leistung, LeistungAdmin) 
 
