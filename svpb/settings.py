42d0d51c106077cc6df9bce09bf5713f0cebe001
Holger Karl <hkarl@ieee.org>
2014-04-20 16:42:22 +0200

    playing around with CSS files a bit

--- svpb/settings.py
+++ svpb/settings.py
@@ -76,7 +76,11 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    # os.path.join (APPLICATION_DIR, '..', 'arbeitsplan', 'static'),
+    os.path.join (APPLICATION_DIR, '..', 'templates'),
 )
+
+print STATICFILES_DIRS
 
 # List of finder classes that know how to find static files in
 # various locations.
