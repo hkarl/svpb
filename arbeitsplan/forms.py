5283f6f4a252b7abd8e8001a15e26c8b7946ad6d
Holger Karl <hkarl@ieee.org>
2014-04-28 20:21:47 +0200

    create Leistung mit crispy forms

--- arbeitsplan/forms.py
+++ arbeitsplan/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 import models
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Button
+from crispy_forms.bootstrap import StrictButton, FormActions
+
+
 class MeldungForm (forms.Form):
 
     def __init__ (self, *args, **kwargs):
@@ -25,6 +30,14 @@ class MeldungForm (forms.Form):
                 
 
 class CreateLeistungForm (forms.ModelForm):
+
+    def __init__ (self, *args, **kwargs):
+        super(CreateLeistungForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = ""
+        self.helper.form_method = "post"
+        self.helper.add_input (Submit ('apply', 'Eintragen'))
+        
     class Meta:
         model = models.Leistung
         exclude = ('melder',
@@ -36,9 +49,7 @@ class CreateLeistungForm (forms.ModelForm):
             
 ##################################
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Button
-from crispy_forms.bootstrap import StrictButton, FormActions
+
 
 class NameFilterForm (forms.Form):
     def __init__(self, *args, **kwargs):
@@ -53,6 +64,7 @@ class NameFilterForm (forms.Form):
         self.helper.form_class = "form-inline"
         ## self.helper.label_class = "col-lg-2"
         ## self.helper.field_class = "col-lg-8"
+        # self.helper.help_text_inline = True
         self.helper.field_template = "bootstrap3/layout/inline_field.html"
         self.helper.layout = Layout(
             'last_name',
