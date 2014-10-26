19798babdf29a680b3efe47eae667784909c8b4b
Holger Karl <hkarl@ieee.org>
2014-10-26 15:55:14 +0100

    yaml has a bug in its timezone aware parsing methods, creates needless warning. therefore, swithcing reluctantly to json

--- dump.sh
+++ dump.sh
@@ -1,8 +1,8 @@
-python manage.py  dumpdata auth.User --format yaml > arbeitsplan/fixtures/user.yaml
-python manage.py  dumpdata arbeitsplan --format yaml > arbeitsplan/fixtures/arbeitsplan.yaml
-python manage.py  dumpdata post_office --format yaml > arbeitsplan/fixtures/post_office.yaml
-python manage.py  dumpdata auth.Group  --format yaml > arbeitsplan/fixtures/groups.yaml
+python manage.py  dumpdata auth.User --format json > arbeitsplan/fixtures/user.json
+python manage.py  dumpdata arbeitsplan --format json > arbeitsplan/fixtures/arbeitsplan.json
+python manage.py  dumpdata post_office --format json > arbeitsplan/fixtures/post_office.json
+python manage.py  dumpdata auth.Group  --format json > arbeitsplan/fixtures/groups.json
 cd arbeitsplan/fixtures 
-tar cfz ../../svpbbackup.tgz *yaml 
+tar cfz ../../svpbbackup.tgz *json 
 
-# cat user.yaml arbeitsplan.yaml groups.yaml post_office.yaml > initial_data.yaml 
+# cat user.json arbeitsplan.json groups.json post_office.json > initial_data.json 
