0e9c004c346f8084cd9a420892ba8297a912e6df
Holger Karl <hkarl@ieee.org>
2014-10-26 15:36:39 +0100

    dumping into initial data pointless; create a tgz file instead for backup

--- dump.sh
+++ dump.sh
@@ -3,4 +3,6 @@ python manage.py  dumpdata arbeitsplan --format yaml > arbeitsplan/fixtures/arbe
 python manage.py  dumpdata post_office --format yaml > arbeitsplan/fixtures/post_office.yaml
 python manage.py  dumpdata auth.Group  --format yaml > arbeitsplan/fixtures/groups.yaml
 cd arbeitsplan/fixtures 
-cat user.yaml arbeitsplan.yaml groups.yaml post_office.yaml > initial_data.yaml 
+tar cfz ../../svpbbackup.tgz *yaml 
+
+# cat user.yaml arbeitsplan.yaml groups.yaml post_office.yaml > initial_data.yaml 
