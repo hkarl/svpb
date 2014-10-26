079bb9e377aeeb0cfbbaba0f27e8143125b83e32
Holger Karl <hkarl@ieee.org>
2014-10-26 18:10:58 +0100

    Testing for duplicates in the stundenplan

--- arbeitsplan/tests.py
+++ arbeitsplan/tests.py
@@ -8,6 +8,7 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.test import Client
+from arbeitsplan.models import *
 
 class SimpleTest(TestCase):
     fixtures=['arbeitsplan/fixtures/arbeitsplan.json',
@@ -42,10 +43,10 @@ class SimpleTest(TestCase):
                         response.status_code == 302)
         return cl
 
-    def test_login_works(self):
+    def _test_login_works(self):
         self.login_user('hkarl')
 
-    def test_leistung_bearbeiten(self):
+    def _test_leistung_bearbeiten(self):
         """
         According to github report #14, lesitung bearbeiten fails. 
         """
@@ -57,5 +58,61 @@ class SimpleTest(TestCase):
         # and with some qualifiers:
         response = cl.get('/arbeitsplan/leistungenBearbeiten/z=me/?last_name=&first_name=&aufgabengruppe=3&von=&bis=&status=OF&status=RU&filter=Filter+anwenden')
         self.assertEqual(response.status_code, 200)
+
+    def _test_changes_in_stundenplan(self):
+        cl = self.login_user('JochenMelzian')
+        response = cl.get('/arbeitsplan/stundenplaene/6/')
+        print response
+        self.assertEqual(response.status_code, 200)
+
+
+    def check_stundenplaene_unique(self):
+        problem = False
+
+        for a in Aufgabe.objects.all():
+            s = Stundenplan.objects.filter(aufgabe=a)
+            for u in range(8,24):
+                s2 = s.filter(uhrzeit=u)
+                c = s2.count()
+                if c>1:
+                    problem = True
+                    ## print a, u, s2.count()
+                    ## print [x.anzahl for x in s2]
+
+        return problem
+
+    def test_stundenplaene_unique(self):
+        """for all comubinations of Aufgabe and uzhrzeit,
+        there must be at most ONE entry in stundenplaene"""
+
+        problem1 = self.check_stundenplaene_unique()
+
+        # try to kick out all the redundant combiantions:
+        if problem1:
+            for a in Aufgabe.objects.all():
+                for u in range(8,24):
+                    s = Stundenplan.objects.filter(aufgabe=a).filter(uhrzeit=u)
+                    c = s.count()
+                    if c > 1:
+                        tmp = [x for x in s]
+                        print c
+                        for d in tmp[:-1]:
+                            print d
+                            d.delete()
+
+            problem2 = self.check_stundenplaene_unique()
+
+            if problem2:
+                print "Problem persisted after clean up - giving up"
+                self.assertFalse(problem2)
+            else:
+                # dumping the sanitized data
+                from django.core.management import call_command
+                output=open("sanitized.json", "w")
+                call_command('dumpdata', 'arbeitsplan', format='json',
+                             indent=3,stdout=output)
+                output.close()
+
+        self.assertFalse(False)
 
 
