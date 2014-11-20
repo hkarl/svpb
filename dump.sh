bcf844610bfd35606f7699fc754cf026136d5bcf
Holger Karl <hkarl@ieee.org>
2014-11-20 21:31:40 +0100

    use dump to store encrpyted backup in dropbx

--- dump.sh
+++ dump.sh
@@ -3,6 +3,10 @@ python manage.py  dumpdata arbeitsplan --format json > arbeitsplan/fixtures/arbe
 python manage.py  dumpdata post_office --format json > arbeitsplan/fixtures/post_office.json
 python manage.py  dumpdata auth.Group  --format json > arbeitsplan/fixtures/groups.json
 cd arbeitsplan/fixtures 
-tar cfz ../../svpbbackup.tgz *json 
+tar cfz svpbbackup.tgz *json 
+echo XXXX | ccrypt -f -k - svpbbackup.tgz
+cp svpbbackup.tgz.cpt /home/svpb/Dropbox/svpb
+
+
 
 # cat user.json arbeitsplan.json groups.json post_office.json > initial_data.json 
