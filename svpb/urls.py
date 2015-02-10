a33305ccdb285903efe199a9ce43ccec7844fda0
Holger Karl <hkarl@ieee.org>
2015-02-10 21:56:06 +0100

    all home URLs should work

--- svpb/urls.py
+++ svpb/urls.py
@@ -38,8 +38,8 @@ urlpatterns = patterns('',
          ), 
 
     url (r'^home/$',
-         TemplateView.as_view (template_name='home.html'),
-         name="home",
+         arbeitsplan.views.HomeView.as_view (template_name='home.html'),
+         name="home2",
          ),
 
     url (r'^keinVorstand/$',
