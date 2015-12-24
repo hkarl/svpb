7a63a17145804e465b3f6ba2e329fb17f5c2864b
Holger Karl <hkarl@ieee.org>
2015-12-24 12:03:31 +0100

    Use "/login" as default URL where to redirect URLs when not logged in.
     Could use "/" as alternative as well...

--- svpb/activeTest.py
+++ svpb/activeTest.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 def active_and_login_required(function=None,
                               redirect_field_name=REDIRECT_FIELD_NAME,
-                              login_url=None):
+                              login_url="/login"):
     actual_decorator = user_passes_test(
         lambda u: u.is_authenticated() and u.is_active,
         login_url=login_url,
