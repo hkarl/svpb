c0d4f1b3237798d8db901022b6b8c09f40130004
Holger Karl <hkarl@ieee.org>
2016-04-23 16:41:04 +0200

    need to dump the entire database to prevent integrity errors when reading back in.

--- dump.sh
+++ dump.sh
@@ -2,7 +2,9 @@ python manage.py  dumpdata auth.User --format json > arbeitsplan/fixtures/user.j
 python manage.py  dumpdata arbeitsplan --format json > arbeitsplan/fixtures/arbeitsplan.json
 python manage.py  dumpdata post_office --format json > arbeitsplan/fixtures/post_office.json
 python manage.py  dumpdata auth.Group  --format json > arbeitsplan/fixtures/groups.json
-cd arbeitsplan/fixtures 
+python manage.py dumpdata --exclude auth.permission --exclude contenttypes > arbeitsplan/fixtures/db.json
+arbeitsplan/fixtures/
+cd arbeitsplan/fixtures
 tar cfz svpbbackup.tgz *json 
 echo XXXX | ccrypt -f -k - svpbbackup.tgz
 cp svpbbackup.tgz.cpt /home/svpb/Dropbox/svpb
