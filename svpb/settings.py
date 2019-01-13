784cc462e95f0394e1dc79eee1249c250fa1e9b9
Holger Karl <hkarl@ieee.org>
2019-01-13 12:05:50 +0100

    wieder online

--- svpb/settings.py
+++ svpb/settings.py
@@ -25,7 +25,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'svpbdata',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'svpbdb',
