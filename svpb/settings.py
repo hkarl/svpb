efb08e7675a7fb8e5133edf91127e0896311cfa9
Holger Karl <hkarl@ieee.org>
2019-12-22 18:45:59 +0100

    debugging

--- svpb/settings.py
+++ svpb/settings.py
@@ -37,6 +37,8 @@ DATABASES = {
     }
 }
 
+# print(f"DATABASE: {DATABASES['default']['ENGINE']}")
+    
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = [".svpb.de"]
