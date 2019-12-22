ffb1d69a812663fda4c35f8e95f8959469d43e28
Holger Karl <hkarl@ieee.org>
2019-12-22 18:23:01 +0100

    key updated

--- svpb/settings.py
+++ svpb/settings.py
@@ -31,7 +31,7 @@ DATABASES = {
         'NAME': 'svpbdata',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'svpbdb',
-        'PASSWORD': 'Eef1Aef3',
+        'PASSWORD': 'XXXXX',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
