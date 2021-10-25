479540b436f731d8028eef3df7e831a091763734
Miroslaw Dynia <mdynia@gmail.com>
2021-10-25 13:41:10 +0200

    Exception Value:
    'bool' object is not callable

--- svpb/activeTest.py
+++ svpb/activeTest.py
@@ -5,7 +5,7 @@ def active_and_login_required(function=None,
                               redirect_field_name=REDIRECT_FIELD_NAME,
                               login_url="/login"):
     actual_decorator = user_passes_test(
-        lambda u: u.is_authenticated() and u.is_active,
+        lambda u: u.is_authenticated and u.is_active,
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
